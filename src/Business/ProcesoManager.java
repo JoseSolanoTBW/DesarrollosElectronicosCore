5615ec09df113fc3c337fa468248dd9664314d17
JoseSolano <jsolanom>
2018-05-26 20:49:33 -0600

    Tramites

--- src/Business/ProcesoManager.java
+++ src/Business/ProcesoManager.java
@@ -2,7 +2,9 @@ package Business;
 
 import java.util.ArrayList;
 
+import DataAccess.MongoDbTransaction;
 import Objects.Grupo;
+import Objects.Pregunta;
 import Objects.Tarea;
 import Objects.Tramite;
 
@@ -17,15 +19,39 @@ public class ProcesoManager {
 		return nuevoTramite;
 	}
 	
-	public Tramite AddStepsToProcess(Tramite tramite) 
+	public Tramite AddStepsToProcess(Tramite tramite, String descripcion, String nombreGrupo ) 
 	{
 		Tarea pendingTask = new Tarea();
 		Grupo group = new Grupo();
 		
+		pendingTask.setDescripcion(descripcion);
+		group.setNombre(nombreGrupo);
+		
 		pendingTask.setGrupoUsuarios(group);
+		tramite.setTarea(pendingTask);		
 		
-		return tramite;
+		return tramite;		
+	}
+	
+	public Tramite AddQuestions(Tramite tramite, String pregunta, ArrayList<String> respuestas, String tipo) 
+	{
+		ArrayList<Tarea> tareas = tramite.getTareas();
+		Tarea lastTarea = tareas.get(tareas.size() - 1);
+		Pregunta newPregunta = new Pregunta();
 		
+		newPregunta.setPregunta(pregunta);
+		newPregunta.setRespuestas(respuestas);
+		newPregunta.setTipo(tipo);
+		
+		lastTarea.setPregunta(newPregunta);	
+		
+		return tramite;		
+	}
+	
+	public void SaveProcess(Tramite traminte) 
+	{
+		MongoDbTransaction transaction = new MongoDbTransaction();
+		transaction.InsertDocuement(traminte, "Tramites");
 	}
 	
 }
